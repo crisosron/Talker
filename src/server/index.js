638811e5fc8dddd09bc8dd569c3f69ae51f94a5e
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-07-05 18:04:03 +1200

    Basic client and server setup

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let socket = require('socket.io');
+let serverConstants = require('./server-constants');
+let app = express();
+
+//Server setup
+let server = app.listen(serverConstants.PORT_NUM, () => {
+    console.log(`Listening to requests on port num: ${serverConstants.PORT_NUM}`);
+});
+
+//Serves everything in src/client to the client side
+app.use(express.static('src/client'));
+
+//Creating server socket
+let io = socket(server);
+
+//Connection handling
+io.on('connect', (clientSocket) => {
+    console.log(`Connection with client established: ${clientSocket.id}`);
+
+    //Client disconnection handling
+    clientSocket.on('disconnect', () => {
+        console.log('Connection with client lost');
+    });
+});
---
+++ src/server/server-constants.js
@@ -0,0 +1,5 @@
+const PORT_NUM = 5409;
+
+module.exports = {
+    PORT_NUM: PORT_NUM
+};
