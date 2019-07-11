b285af22c666a7b04383361d45095220064ffb06
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-07-11 14:49:14 +1200

    Split some handlers into their own functions

--- src/client/client.js
+++ src/client/client.js
@@ -9,11 +9,8 @@ const chatOutputDiv = document.getElementById('chat-output');
 const confirmNameButton = document.getElementById('confirm-name-button');
 const usersBar = document.getElementById('users-bar');
 
-confirmNameButton.addEventListener('click', () => {
-    let userName = userNameBar.value;
-    if(userName == "") alert("Enter a valid username!");
-    else socket.emit('registerUserName', {userName: userName});
-});
+confirmNameButton.addEventListener('click', confirmUserName);
+sendButton.addEventListener('click', sendMessage);
 
 //Handles events where Enter key is pressed
 document.addEventListener('keydown', (e) => {
@@ -25,7 +22,41 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-sendButton.addEventListener('click', () => {
+//Server confirms name validity of this client
+socket.on('userNameRegisteredStatus', (data) => {
+    userNameRegisteredStatus(data);
+});
+
+//Handling for when the server requests for all clients to create a new user bar item
+socket.on('createUserBarItem', (data) => {
+    let userName = data.userName;
+    createUserBarItem(userName);
+});
+
+
+socket.on('displayMessage', (data) => {
+    displayMessage(data);
+});
+
+/**
+ * Creates a user bar item object
+ * @param {string} userName Username of the client associted with the user bar item to be created
+ */
+function createUserBarItem(userName){
+    usersBar.innerHTML += `<div class="user-bar-item this-sender" id="${userName}">
+                              <p>${userName}</p>        
+                           </div>`;
+    const userBarDiv = document.getElementById(userName);
+    let userBarItem = new UserBarItem(userName, userBarDiv, socket.id);
+    userBarItems.push(userBarItem);
+    console.log(`Created a user bar for the user name: ${userName}`);  
+}
+
+/**
+ * Sends a message to all clients
+ */
+function sendMessage(){
+    console.log('sendMessage function called');
     let textToSend = textBar.value;
     let senderName = userNameBar.value;
 
@@ -43,11 +74,22 @@ sendButton.addEventListener('click', () => {
     });
 
     textBar.value = "";
+}
 
-});
+/**
+ * Emits a message to the server to register the user name of this client
+ */
+function confirmUserName(){
+    let userName = userNameBar.value;
+    if(userName == "") alert("Enter a valid username!");
+    else socket.emit('registerUserName', {userName: userName});
+}
 
-//Server confirms name validity of this client
-socket.on('userNameRegisteredStatus', (data) => {
+/**
+ * Checks if the user name has been successfully registered by the server
+ * @param {Object} data Object that contains a boolean value that indicates if the user name is valid
+ */
+function userNameRegisteredStatus(data){
     let validUserName = data.validUserName;
     if(validUserName) {
         alert(`Username registered as: ${userNameBar.value}`);
@@ -56,16 +98,13 @@ socket.on('userNameRegisteredStatus', (data) => {
         });
     }
     else alert(`Duplicate username, please enter a different username`);
-});
+}
 
-//Handling for when the server requests for all clients to create a new user bar item
-socket.on('createUserBarItem', (data) => {
-    let userName = data.userName;
-    createUserBarItem(userName);
-});
-
-
-socket.on('displayMessage', (data) => {
+/**
+ * Displays the message sent from a client to this clients viewport
+ * @param {Object} data Object that holds information about the message being sent
+ */
+function displayMessage(data){
     let message = data.message;
     let senderName = data.senderName;
     let senderSocketID = data.senderSocketID;
@@ -73,18 +112,4 @@ socket.on('displayMessage', (data) => {
     //Highlights the username in blue if the message being displayed is from this client, else highlight the username red
     if(senderSocketID == socket.id)chatOutputDiv.innerHTML += `<p><strong class="this-sender">${senderName}</strong>: ${message}</p><br>`;
     else chatOutputDiv.innerHTML += `<p><strong class="other-sender">${senderName}</strong>: ${message}</p><br>`;
-});
-
-/**
- * Creates a user bar item object
- *
- */
-function createUserBarItem(userName){
-    usersBar.innerHTML += `<div class="user-bar-item this-sender" id="${userName}">
-                              <p>${userName}</p>        
-                           </div>`;
-    const userBarDiv = document.getElementById(userName);
-    let userBarItem = new UserBarItem(userName, userBarDiv, socket.id);
-    userBarItems.push(userBarItem);
-    console.log(`Created a user bar for the user name: ${userName}`);  
 }
--- src/server/index.js
+++ src/server/index.js
@@ -23,38 +23,71 @@ io.on('connect', (clientSocket) => {
 
     //Handling for when a client tries to enter a user name
     clientSocket.on('registerUserName', (data) => {
-        let userName = data.userName;
-
-        //Sends message to specific client if the client tries to register a user name that is already in use
-        if(userNames.includes(userName))clientSocket.emit('userNameRegisteredStatus', {validUserName: false});
-        else {
-            clientSocket.emit('userNameRegisteredStatus', {validUserName: true});
-            userNames.push(userName);    
-        }
+        registerUserName(data, clientSocket);
     });
 
     //Handling for when a user bar item needs to be created
     clientSocket.on('createUserBarItem', (data) => {
-        io.sockets.emit('createUserBarItem', {
-            userName: data.userName
-        });
+        createUserBarItem(data);
     });
 
     //Handling for when a client sends a message
     clientSocket.on('messageSent', (data) => {
-
-        //Emitting to all clients connected to this server to display a message sent by a client
-        io.sockets.emit('displayMessage', {
-            message: data.message,
-            senderName: data.senderName,
-            senderSocketID: clientSocket.id //To identify who sent the message
-        });
-
+        messageSent(data, clientSocket.id);
     });
 
     //Client disconnection handling
     clientSocket.on('disconnect', () => {
-        console.log('Connection with client lost');
+        disconnectClient(clientSocket.id);
     });
 
 });
+
+/**
+ * Registers the user name of the client
+ * @param {Object} data Object that holds the username of the client
+ */
+function registerUserName(data, clientSocket){
+    let userName = data.userName;
+
+    //Sends message to specific client if the client tries to register a user name that is already in use
+    if(userNames.includes(userName))clientSocket.emit('userNameRegisteredStatus', {validUserName: false});
+    else {
+        clientSocket.emit('userNameRegisteredStatus', {validUserName: true});
+        userNames.push(userName);    
+    }
+}
+
+/**
+ * Sends a message to all clients to create a user bar item
+ * @param {Object} data Object that holds the user name that corresponds with the UserBarItem to be created
+ */
+function createUserBarItem(data){
+    //Emitting to all clients connected to this server to display a message sent by a client
+    io.sockets.emit('createUserBarItem', {
+        userName: data.userName
+    });
+}
+
+/**
+ * Processes the message sending across the clients
+ * @param {Object} data Holds information of the sender and the message being sent
+ * @param {string} clientSocketID Socket id of the sender
+ */
+function messageSent(data, clientSocketID){
+
+     //Emitting to all clients connected to this server to display a message sent by a client
+     io.sockets.emit('displayMessage', {
+        message: data.message,
+        senderName: data.senderName,
+        senderSocketID: clientSocketID //To identify who sent the message
+    });
+}
+
+/**
+ * Operations when a client disconnects
+ * @param {string} clientSocketID Socket id of the client that disconnected
+ */
+function disconnectClient(clientSocketID){
+    console.log('Connection with client lost');
+}
